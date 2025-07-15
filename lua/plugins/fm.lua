1f03675294f013c193529e5fed1126709947618d
ranoot <reynardseow26@gmail.com>
2025-07-15 17:58:36 +0800

    <leader>ff now opens the finder in the buffer's directory

--- lua/plugins/fm.lua
+++ lua/plugins/fm.lua
@@ -32,13 +32,22 @@ return {
     {
         'nvim-telescope/telescope.nvim',
         branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = { 'nvim-lua/plenary.nvim', 'stevearc/oil.nvim' },
         config = function ()
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope live grep' })
-            -- vim.keymap.set('n', '<leader>ff', builtin.find_files({
-            --     cwd = require("oil").get_current_dir(),
-            -- }), { desc = 'Telescope find files in current Oil directory' })
+            -- vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope (F)ind (F)iles' })
+            vim.keymap.set('n', '<leader>ff', function ()
+                local curdir = require('oil').get_current_dir()
+                if curdir=="" then
+                    local bufname = vim.api.nvim_buf_get_name(0);
+                    curdir = vim.fn.fnamemodify(bufname, ":h");
+                end
+                if curdir=="" then
+                    curdir = vim.fn.getcwd()
+                end
+
+                builtin.find_files({ cwd = curdir })
+            end, { desc = 'Telescope find files in current Oil directory' })
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
             vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
             vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'Telescope find document symbols' })
